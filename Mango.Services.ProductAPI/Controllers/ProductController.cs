b03fdd00062e4d6bd8c472d12d29c070217ad95e
Glailton Costa <francisco.glailton@ambevtech.com.br>
2021-08-06 00:14:24 -0300

    Creating Product MS

---
+++ Mango.Services.ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,116 @@
+﻿using Mango.Services.ProductAPI.Models.Dtos;
+using Mango.Services.ProductAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Mango.Services.ProductAPI.Controllers
+{
+    [Route("api/products")]
+    public class ProductController : ControllerBase
+    {
+        protected ResponseDto _response;
+        private readonly IProductRepository _productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+            this._response = new ResponseDto();
+        }
+
+        [HttpGet]
+        public async Task<object> Get()
+        {
+            try
+            {
+                IEnumerable<ProductDto> productDtos = await _productRepository.GetProducts();
+                _response.Result = productDtos;
+
+            }
+            catch(Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.Message };
+            }
+
+            return _response;
+        }
+
+        [HttpGet]
+        [Route("{productId}")]
+        public async Task<object> GetById(int productId)
+        {
+            try
+            {
+                var productDto = await _productRepository.GetProductById(productId);
+                _response.Result = productDto;
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.Message };
+            }
+
+            return _response;
+        }
+
+        [HttpPost]
+        public async Task<object> Post([FromBody] ProductDto productDto)
+        {
+            try
+            {
+                var model = await _productRepository.CreateUpdateProduct(productDto);
+                _response.Result = model;
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.Message };
+            }
+
+            return _response;
+        }
+
+        [HttpPut]
+        public async Task<object> Put([FromBody] ProductDto productDto)
+        {
+            try
+            {
+                var model = await _productRepository.CreateUpdateProduct(productDto);
+                _response.Result = model;
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.Message };
+            }
+
+            return _response;
+        }
+
+
+        [HttpDelete]
+        [Route("{productId}")]
+        public async Task<object> Put(int productId)
+        {
+            try
+            {
+                var result = await _productRepository.DeleteProduct(productId);
+                _response.Result = result;
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.Message };
+            }
+
+            return _response;
+        }
+
+    }
+}
---
+++ Mango.Web/Services/IServices/IBaseService.cs
@@ -0,0 +1,15 @@
+﻿using Mango.Services.ProductAPI.Models.Dtos;
+using Mango.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mango.Web.Services.IServices
+{
+    public interface IBaseService : IDisposable
+    {
+        ResponseDto responseModel { get; set; }
+        Task<T> SendAsync<T>(ApiRequest apiRequest);
+    }
+}
