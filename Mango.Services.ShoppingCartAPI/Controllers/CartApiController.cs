31f7a8e6872a5e281f866dbc5180eb8b876f26c8
Glailton Costa <francisco.glailton@ambevtech.com.br>
2021-09-01 23:07:38 -0300

    Creating Shopping Cart Api

---
+++ Mango.Services.ShoppingCartAPI/Controllers/CartApiController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Mango.Services.ShoppingCartAPI.Models.Dto;
+using Mango.Services.ShoppingCartAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.ShoppingCartAPI.Controllers
+{
+    [ApiController]
+    [Route("api/cart")]
+    public class CartApiController : Controller
+    {
+        private readonly ICartRepository _cartRepository;
+        private ResponseDto _response;
+
+        public CartApiController(ICartRepository cartRepository)
+        {
+            _cartRepository = cartRepository;
+            this._response = new ResponseDto();
+        }
+
+        [HttpGet("/{userId}")]
+        public async Task<object> GetCart(string userId)
+        {
+            try
+            {
+                CartDto cartDto = await _cartRepository.GetCartByUserId(userId);
+                _response.Result = cartDto;
+            }
+            catch (Exception e)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { e.ToString() };
+            }
+
+            return _response;
+        }
+        
+        [HttpPost("AddCart")]
+        public async Task<object> AddCart([FromBody] CartDto cartDto)
+        {
+            try
+            {
+                CartDto cartDtoFromDb = await _cartRepository.CreateUpdateCart(cartDto);
+                _response.Result = cartDtoFromDb;
+            }
+            catch (Exception e)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { e.ToString() };
+            }
+
+            return _response;
+        }
+        
+        [HttpPost("UpdateCart")]
+        public async Task<object> UpdateCart([FromBody] CartDto cartDto)
+        {
+            try
+            {
+                CartDto cartDtoFromDb = await _cartRepository.CreateUpdateCart(cartDto);
+                _response.Result = cartDtoFromDb;
+            }
+            catch (Exception e)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { e.ToString() };
+            }
+
+            return _response;
+        }
+        
+        [HttpPost("RemoveCart")]
+        public async Task<object> RemoveCart([FromBody] int cartDetailsId)
+        {
+            try
+            {
+                Boolean isSuccess = await _cartRepository.RemoveFromCart(cartDetailsId);
+                _response.Result = isSuccess;
+            }
+            catch (Exception e)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { e.ToString() };
+            }
+
+            return _response;
+        }
+    }
+}
---
+++ Mango.Services.ShoppingCartAPI/DbContext/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Mango.Services.ShoppingCartAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.ShoppingCartAPI.DbContext
+{
+    public class ApplicationDbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        protected ApplicationDbContext()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<CartHeader> CartHeaders { get; set; }
+        public DbSet<CartDetails> CartDetails { get; set; }
+    }
+}
