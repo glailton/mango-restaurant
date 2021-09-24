a589b5ccce19d0fb0344d481491465a0258399e5
Glailton Costa <francisco.glailton@ambevtech.com.br>
2021-09-23 22:56:03 -0300

    Create OrderApi project

---
+++ Mango.Services.OrderAPI/DbContext/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Mango.Services.OrderAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.OrderAPI.DbContext
+{
+    public class ApplicationDbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        protected ApplicationDbContext()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetails> OrderDetails { get; set; }
+    }
+}
---
+++ Mango.Services.OrderAPI/Migrations/20210924015252_AddPickupDateTimeCollumn.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Mango.OrderAPI.Migrations
+{
+    public partial class AddPickupDateTimeCollumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PickupDateTime",
+                table: "OrderHeaders",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PickupDateTime",
+                table: "OrderHeaders");
+        }
+    }
+}
